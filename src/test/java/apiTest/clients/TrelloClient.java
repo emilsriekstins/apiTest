6145d059c251c9f1328586d683b18f558d5097d2
emilsriekstins <144341800+emilsriekstins@users.noreply.github.com>
2023-09-14 13:08:44 +0300

    Bug fix

--- src/test/java/apiTest/clients/TrelloClient.java
+++ src/test/java/apiTest/clients/TrelloClient.java
@@ -3,22 +3,17 @@ package apiTest.clients;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 import static apiTest.constants.ProjectConstants.API_KEY;
 import static apiTest.constants.ProjectConstants.API_TOKEN;
 
 public class TrelloClient {
-    private static RequestSpecification trelloSpec() {
-        return RestAssured
-                .given().log().all()
-                .queryParam("key", API_KEY)
-                .queryParam("token", API_TOKEN);
-    }
-
     public static Response getBoardInfo(String boardID){
         return RestAssured
-                .given(trelloSpec())
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("key", API_KEY)
+                .queryParam("token", API_TOKEN)
                 .when()
                 .get("https://api.trello.com/1/boards/" + boardID)
                 .then().log().all()
@@ -27,7 +22,10 @@ public class TrelloClient {
     }
     public static Response updateBoardInfo(String boardName, String boardID) {
         return RestAssured
-                .given(trelloSpec())
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("key", API_KEY)
+                .queryParam("token", API_TOKEN)
                 .queryParam("name", boardName)
                 .when()
                 .put("https://api.trello.com/1/boards/" + boardID)
@@ -38,7 +36,10 @@ public class TrelloClient {
 
     public static Response createList(String listName, String boardID) {
         return RestAssured
-                .given(trelloSpec())
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("key", API_KEY)
+                .queryParam("token", API_TOKEN)
                 .queryParam("name", listName)
                 .queryParam("idBoard", boardID)
                 .when()
@@ -50,7 +51,10 @@ public class TrelloClient {
 
     public static Response deleteList(String listID) {
         return RestAssured
-                .given(trelloSpec())
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("key", API_KEY)
+                .queryParam("token", API_TOKEN)
                 .queryParam("value", true)
                 .when()
                 .put("https://api.trello.com/1/lists/" + listID + "/closed")
